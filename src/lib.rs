1b71646570bf595d94395f2e31db06e294495c65
Joshua Goins <josh@redstrate.com>
2025-05-07 19:23:33 -0400

    Remove installer module
    This is no longer used by Astra.

--- src/lib.rs
+++ src/lib.rs
@@ -4,9 +4,5 @@ pub mod existing_dirs;
 /// Reading data from executables
 pub mod execlookup;
 
-/// Initializing a new retail game install from the official retail installer. No execution required!
-#[cfg(feature = "game_install")]
-pub mod installer;
-
 /// Steam ticket blowfish encryption.
 pub mod blowfish;
